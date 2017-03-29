8a9fe9df4b88ab5eee4228ead734daae8a1c3fd3
Juliano <juliano@nationphone.com>
2017-03-29 19:24:11 -0300

    UPDATE - exercicios de webpack

---
+++ ex/index.js
@@ -0,0 +1,3 @@
+import Pessoa from './pessoa'
+const pessoa = new Pessoa('Juliano')
+console.log(pessoa.toString())
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack')
+
+module.exports = {
+    entry: './ex/index.js',
+    output: {
+        path: __dirname + '/public',
+        filename: './bundle.js'
+    },
+    devServer: {
+        port: 8080,
+        contentBase: './public'
+    },
+    module: {
+        loaders: [{
+            test: /.js?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react'],
+                plugins: ['trasform-object-rest-spread']
+            }
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+        }]
+    }
+}
